7988394df0cee85ca62e49599084ce1aea4511d2
Estela Cordeiro Mateus <101813326+stelamateus@users.noreply.github.com>
2022-08-29 12:51:17 -0300

    atribuir função caps nas letras do teclado

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let keys    = document.querySelectorAll('.keyboard_wrapper .key .row span'),
     keyPad  = document.querySelector('.keyboard_wrapper .key'),
     display = document.querySelector('.keyboard_wrapper .display');
 
+    let letters = document.querySelectorAll('.letter')
+
 if(keys && keyPad && display)
 {
     let capsLockMode = false;
@@ -10,24 +12,35 @@ if(keys && keyPad && display)
         key.addEventListener('click', function()
         {
             // console.log(this.innerText);
-            if(this.classList.contains('caps'))
-            {
+            if (this.classList.contains('caps')) {
                 this.classList.toggle('active');
                 keyPad.classList.toggle('uppercase');
 
                 capsLockMode ? capsLockMode = false : capsLockMode = true;
-            }
-            else if (this.classList.contains('backspace')){
+                toggleLetterTo(capsLockMode, letters)
+            } else if (this.classList.contains('backspace')) {
                 let str = display.innerText;
                 display.innerText = str.substring(0, (str.length-1));
-            }else{
-                if(capsLockMode)
-                {
+            } else {
+                if (capsLockMode) {
                     display.innerText += this.dataset.key.toUpperCase();
-                }else{
-                    display.innerText += this.dataset.key.toLowerCase()
+                } else {
+                    display.innerText += this.dataset.key.toLowerCase();
                 }
             }
         });
+    });
+
+
+}
+
+function toggleLetterTo(isUpercase, letters) {
+    letters.forEach(element => {
+        console.log(element.innerHTML.toUpperCase())
+        if (isUpercase) {
+            element.classList.add("uppercased")
+        } else {
+            element.classList.remove("uppercased")
+        }
     });
 }
